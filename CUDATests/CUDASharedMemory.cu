6aca2ea4e09e9a0c7e2b45b633e9247c2be650ed
kdedeurw <66272745+kdedeurw@users.noreply.github.com>
2022-01-13 22:17:22 +0100

    CUDA Test Solution

---
+++ CUDATests/CUDASharedMemory.cu
@@ -0,0 +1,11 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include "CUDASharedMemory.cuh"
+
+using namespace CUDASharedMemory;
+
+__global__ void Test()
+{
+
+}
---
+++ CUDATests/CUDASharedMemory.cuh
@@ -0,0 +1,25 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+namespace CUDASharedMemory
+{
+	__global__ void staticReverse(int* d, int n)
+	{
+		__shared__ int s[64];
+		int t = threadIdx.x;
+		int tr = n - t - 1;
+		s[t] = d[t];
+		//__syncthreads();
+		d[t] = s[tr];
+	}
+
+	__global__ void dynamicReverse(int* d, int n)
+	{
+		extern __shared__ int s[];
+		int t = threadIdx.x;
+		int tr = n - t - 1;
+		s[t] = d[t];
+		//__syncthreads();
+		d[t] = s[tr];
+	}
+}
